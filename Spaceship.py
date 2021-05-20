c2529b507b4fbcf69297f17115c6f81a2db81dda
nardiyansah <janatri1398@gmail.com>
2021-05-20 19:27:39 +0700

    Update Spaceship.py

--- Spaceship.py
+++ Spaceship.py
@@ -1,13 +1,18 @@
 import pygame
 
+from Bullet import Bullet
+
 
 class Spaceship:
-	movementSpeed = 0.1
+	movementSpeed = 2.3
+	spaceship_images = ['./assets/spaceship_1.png', './assets/spaceship_2.png', './assets/spaceship_3.png']
 
 	def __init__(self, screen):
-		self.__playerImg = pygame.image.load('./assets/spaceship64.png')
+		self.__playerImg = pygame.image.load(self.spaceship_images[0])
 		self.__playerX = 368
 		self.__playerY = 500
+		self.__heart = 3
+		self.__level = 0
 		self.screen = screen
 
 	# show player
@@ -15,7 +20,34 @@ class Spaceship:
 		self.screen.blit(self.__playerImg, (self.__playerX, self.__playerY))
 
 	def move_to_right(self):
-		self.__playerX += self.movementSpeed
+		if self.__playerX + self.movementSpeed < 738:
+			self.__playerX += self.movementSpeed
 
 	def move_to_left(self):
-		self.__playerX -= self.movementSpeed
+		if self.__playerX - self.movementSpeed > 0:
+			self.__playerX -= self.movementSpeed
+
+	def getX(self):
+		return self.__playerX
+
+	def getY(self):
+		return self.__playerY
+
+	def getHeart(self):
+		return self.__heart
+
+	def level_up(self):
+		if self.__level < 2:
+			self.__level += 1
+			self.__playerImg = pygame.image.load(self.spaceship_images[self.__level])
+
+	def reset_level(self):
+		self.__level = 0
+		self.__playerImg = pygame.image.load(self.spaceship_images[self.__level])
+
+	def shoot(self):
+		return Bullet(self.screen, self.__playerX + 24, self.__playerY, self.__level)
+
+	def lose_one_heart(self):
+		self.__heart -= 1
+
