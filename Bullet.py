40dd69ad9f3771d56695ab3eac7d9c9be2a48ecb
Latisya <50728971+Latisya@users.noreply.github.com>
2021-05-20 19:27:32 +0700

    Class Bullet / peluru

---
+++ Bullet.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Bullet:
+	movementSpeed = 3.2
+	bullet_images = ['./assets/bullet_1.png', './assets/bullet_2.png', './assets/bullet_3.png']
+
+	def __init__(self, screen, X, Y, level):
+		self.__bulletImg = pygame.image.load(self.bullet_images[level])
+		self.__bulletX = X
+		self.__bulletY = Y
+		self.screen = screen
+
+	def move(self):
+		self.__bulletY -= self.movementSpeed
+
+	def show(self):
+		self.screen.blit(self.__bulletImg, (self.__bulletX, self.__bulletY))
+
+	def getX(self):
+		return self.__bulletX
+
+	def getY(self):
+		return self.__bulletY
